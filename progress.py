f941cab8fc5149685a118efb53818ae43fad60a6
uniqname <lijame@umich.edu>
2020-06-29 23:07:19 -0400

    adding to progress

--- progress.py
+++ progress.py
@@ -1,1 +1,15 @@
 #python program to display progress
+
+import time
+import datetime as dt
+import tkinter as tk
+from tkinter import messagebox
+import winsound
+
+
+e = Entry(master)
+e.pack()
+e.delete(0, END)
+e.insert(0, "a default value")
+
+print()
